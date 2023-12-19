11a3991bb91295f58fb78cb1896025b39f0592b6
Phong Nguyễn Phi <nguyenphiphong@gmail.com>
2023-12-19 23:53:55 +0700

    first commit

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php 
+define("DOMAIN", "http://localhost/btth02_2");
+define("ROOT", dirname(__FILE__, 2));
+define("DB_HOST", "localhost");
+define("DB_NAME", "btth03");
+define("DB_USER", "root");
+define("DB_PASS", "");
